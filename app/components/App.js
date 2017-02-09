6e716f9ce8a5efacb184d6bb1f79414866a6bada
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-09 17:17:09 +0000

    Centers Title text

--- app/components/App.js
+++ app/components/App.js
@@ -2,10 +2,14 @@ var React = require('react');
 var Profile = require('./Profile');
 var Cover = require('./Cover');
 
+var appStyle = {
+  textAlign: 'center'
+};
+
 var App = React.createClass({
      render: function (){
           return (
-          	<div>
+          	<div style={appStyle}>
               <Cover />
               <Profile />
             </div>
