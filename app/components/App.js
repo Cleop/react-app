ae03a726e0afd493ebe3efea5aae2c34555ddbc4
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-09 12:29:59 +0000

    Adds Profile component to app component

--- app/components/App.js
+++ app/components/App.js
@@ -1,16 +1,17 @@
 var React = require('react');
-
+var Profile = require('./Profile');
 var picture = "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-alpaca.jpg";
 
-var MyComponentClass = React.createClass({
+var App = React.createClass({
      render: function (){
           return (
           	<div>
-          		<h1>Hello world</h1>
+          		<h1>Cleo Pearson</h1>
         		<img src={picture} style={{height: 200}}/>
+            <Profile />
             </div>
   		);
      }
 });
 
-module.exports = MyComponentClass;
+module.exports = App;
