04419d095dfc6471884a59cda638541c75236b57
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-09 16:10:16 +0000

    Creates file for styling info starting with font

---
+++ app/components/styles.js
@@ -0,0 +1,9 @@
+var fontFamily = 'Roboto, Arial';
+var fontSize   = '4em';
+var padding = '45px 0';
+
+module.exports = {
+  fontFamily: fontFamily,
+  fontSize:   fontSize,
+  padding:    padding,
+}
