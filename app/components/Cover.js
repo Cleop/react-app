c61f022e73e7ef4734a02c1e021a9ea3da2cb2ef
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-09 17:17:24 +0000

    Changes font color to white

--- app/components/Cover.js
+++ app/components/Cover.js
@@ -5,7 +5,8 @@ var coverStyle = {
   background:'black url("http://www.witness-this.com/content/uploads/2016/02/geometric-landscape-design3_by-threetonine.jpg") center center',
   backgroundSize: 'cover',
   overflow: 'hidden',
-  fontSize: '6em',
+  fontSize: '3em',
+  color: 'white',
   fontFamily: styles.fontFamily,
   margin: '0 auto',
   height: '90vh'
