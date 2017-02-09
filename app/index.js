280c07e0d7b018f9438ecc85f42864ffe07b7b9d
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-09 12:29:34 +0000

    Changes name of component to app

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 var ReactDOM = require('react-dom');
-var MyComponentClass = require('./components/App');
+var App = require('./components/App');
 
 ReactDOM.render(
-  <MyComponentClass />,
+  <App />,
   document.getElementById('app')
 );
