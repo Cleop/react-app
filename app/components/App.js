d321c3c619388c2f58d6285de48fe0b561408bfc
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-09 10:45:34 +0000

    Creates first component

---
+++ app/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+var App = React.createClass({
+  render: function() {
+    return <h1>Hello World!</h1>
+  }
+});
+
+module.exports = App;
---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
