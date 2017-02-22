76cf35bb79882262147e35bbd7c9bc156cdede65
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-22 11:29:33 +0000

    Creates centered title row

--- app/components/styling/skillStyles.js
+++ app/components/styling/skillStyles.js
@@ -1,1 +1,23 @@
-// module.exports =
+const titleRow = {
+  position: 'relative',
+  display: 'block',
+  height: '7vh',
+  width: '90vw',
+  margin: 'auto',
+  textAlign: 'center',
+  background: '#ededed'
+};
+
+const row = {
+  position: 'relative',
+  display: 'block',
+  height: '30vh',
+  width: '90vw',
+  margin: 'auto',
+  background: '#e0e0e0'
+};
+
+module.exports = {
+  titleRow,
+  row
+};
