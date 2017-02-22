0301322a3bdcede030c627de3ddc7792456d00b6
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-22 10:47:42 +0000

    Adds styles previously in Profile.js to style page

--- app/components/styles.js
+++ app/components/styles.js
@@ -1,11 +1,50 @@
-var fontFamily = 'Roboto, Arial';
 var fontSize   = '4em';
 var padding = '45px 0';
-var marginAuto = 'auto';
+
+var row = {
+  position: 'relative',
+  display: 'block',
+  height: '30vh',
+  width: '90vw',
+  margin: 'auto',
+  // background: 'grey'
+};
+
+var leftCube = {
+  display: 'inline-block',
+  width: '41vw',
+  height: '100%',
+  padding: '0 2vw',
+  position: 'absolute',
+  left: '0',
+  textAlign: 'center',
+  // background: 'green'
+};
+
+var rightCube = {
+  display: 'inline-block',
+  width: '41vw',
+  height: '100%',
+  padding: '0 2vw',
+  position: 'absolute',
+  right: '0',
+  textAlign: 'center',
+  // background: 'yellow'
+};
+
+var startText = {
+  padding: '12% 0 13.5% 0',
+  width: '80%',
+  margin: 'auto',
+  fontSize: '1.1em',
+  lineHeight: '140%'
+};
 
 module.exports = {
-  fontFamily: fontFamily,
   fontSize:   fontSize,
   padding:    padding,
-  margin: marginAuto
+  row,
+  leftCube,
+  rightCube,
+  startText
 }
