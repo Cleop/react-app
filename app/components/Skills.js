382b45bfa7eb047fe5eb5c6a3969c429bff8cbee
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-22 11:12:13 +0000

    Create skill section file

---
+++ app/components/Skills.js
@@ -0,0 +1,16 @@
+const React = require('react');
+const styles = require('./styling/skillStyles.js');
+
+const row = styles.row;
+
+const Skills = React.createClass({
+  render: function() {
+    return (
+      <div style={row}>
+
+      </div>
+    )
+  }
+});
+
+module.exports = Skills;
